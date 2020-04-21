d9d6ce60782730f2800b6b7985000646244eecc8
Sushi Chanrai <sushi@pathworks.co>
2020-04-21 18:45:31 -0400

    tweak error messaging code on game_history controller

--- app/controllers/api/v1/game_histories_controller.rb
+++ app/controllers/api/v1/game_histories_controller.rb
@@ -12,7 +12,10 @@ class Api::V1::GameHistoriesController < ApplicationController
     game_history.user_id = current_user.id
     if game_history.save
     # render json: {game_history, message: 'Hashes of data will get converted to JSON' }
-    response = { message: "Successfully saved game." }
+    # response = { message: "Successfully saved game." }
+      render json: game_history, status: :accepted
+    else
+      render json: { errors: game_history.errors.full_messages }, status: :unprocessible_entity
     end
   end
 
