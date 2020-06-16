dbb4d34e28f913d935b9883bf853576f85aecd9a
Sushi Chanrai <sushi@pathworks.co>
2020-06-16 18:22:03 -0400

    cleanup comments

--- app/controllers/api/v1/game_histories_controller.rb
+++ app/controllers/api/v1/game_histories_controller.rb
@@ -1,14 +1,12 @@
 class Api::V1::GameHistoriesController < ApplicationController
-  #before_action :find_user, only: [:create]
-  # you would need to tweak the create code in line with @user
+
 
   def index
     game_histories = current_user.game_histories
-    #game_histories = GameHistory.all
     render json: game_histories
   end
 
-#TEST THIS CODE
+
   def create
     game_history = current_user.game_histories.build(game_history_params)
     if game_history.save
@@ -25,8 +23,5 @@ class Api::V1::GameHistoriesController < ApplicationController
       params.permit(:score)
     end
 
-    # def find_user
-    #   @user = User.find(params[:id])
-    # end
 
   end
